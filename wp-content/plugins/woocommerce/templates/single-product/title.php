5b4220e9771b47e5f4af44fb5910874e1326692f
Alexandru Pop <alexandrupop@gmail.com>
2014-01-04 21:07:19 +0200

    Titles

--- wp-content/plugins/woocommerce/templates/single-product/title.php
+++ wp-content/plugins/woocommerce/templates/single-product/title.php
@@ -10,5 +10,4 @@
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 ?>
-<h1 itemprop="name" class="product_title entry-title"><?php the_title(); ?></h1>
-<span class="by-iguelle"></span>
+<h1 itemprop="name" class="product_title entry-title"><?php the_title(); ?><span class="by-iguelle"></span></h1>
